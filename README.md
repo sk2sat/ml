884a7f1b3024298adb2fbbef2c06b100dc878d13
sk2sat <sksat.tec@gmail.com>
2017-01-01 18:35:35 +0900

    なんかエラー出た・・・

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ml
 mail sending program (simple mailing list) by Python 3
+
+this is a 2017-kakizome program.
---
+++ ml.py
@@ -0,0 +1,48 @@
+#!/usr//bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import smtplib
+from email.mime.text import MIMEText
+import datetime
+
+jp = 'iso-2022-jp'
+
+# 設定の読み込み
+set_file = open('setting.cfg', 'r')
+
+# 1行目はFromアドレス
+from_addr = set_file.readline()
+passwd = set_file.readline()
+print('From = ' + from_addr)
+
+# 宛先のリストファイル
+addr_file = open('addrs.lst', 'r')
+
+print(addr_file.readline())
+
+
+# 本文の読み込み
+body_file = open('body.txt', 'r')
+title = body_file.readline()	# タイトルの読み込み
+raw_body = body_file.read()
+
+body = MIMEText(raw_body)
+
+body['Subject'] = 'てすと'
+body['From'] = from_addr
+
+
+print(body)
+
+# メールの送信 (Gmail)
+try:
+	s = smtplib.SMTP('skmt.aesc@gmail.com', 587)
+	s.ehlo()
+	s.starttls()
+	s.ehlo()
+	s.login(from_addr, passwd)
+	s.send_message(body)
+	s.close()
+except Exception:
+	print("Error: can't send mail")
